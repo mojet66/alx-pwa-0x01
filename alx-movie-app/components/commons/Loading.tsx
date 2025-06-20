b05ef2d392acea8d7d62d2491da6ee1527964b7b
mojet66 <oladosuemmanuel2017@gmail.com>
2025-06-20 16:00:35 +0100

    added movies pages

--- alx-movie-app/components/commons/Loading.tsx
+++ alx-movie-app/components/commons/Loading.tsx
@@ -1,7 +1,16 @@
-import React from "react";
-
-const Loading = () => {
-  return <div></div>;
+const Loading: React.FC = () => {
+  return (
+    <div className="z-50 fixed inset-0 flex justify-center items-center bg-black bg-opacity-50">
+      <div className="flex flex-col justify-center items-center bg-white bg-opacity-10 backdrop-blur-md p-8 rounded-lg">
+        <h1 className="mb-4 font-semibold text-white text-4xl md:text-6xl animate-pulse">
+          Loading...
+        </h1>
+        <p className="text-gray-300 text-lg">
+          Please wait, we&apos;re getting next set of movies ready for you.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Loading;
--- alx-movie-app/components/commons/MovieCard.tsx
+++ alx-movie-app/components/commons/MovieCard.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { MovieProps } from "@/interfaces";
+import Image from "next/image";
 
-const MovieCard = () => {
-  return <div></div>;
+const MovieCard: React.FC<MovieProps> = ({
+  title,
+  posterImage,
+  releaseYear,
+}) => {
+  return (
+    <div className="h-[563px]">
+      <div>
+        <Image
+          className="rounded-md w-full h-[430px] hover:cursor-pointer"
+          src={posterImage}
+          width={100}
+          height={100}
+          alt={title}
+        />
+      </div>
+      <div className="flex justify-between py-4">
+        <p className="font-bold text-xl">{title}</p>
+        <p className="text-[#E2D609] text-xl">{releaseYear}</p>
+      </div>
+    </div>
+  );
 };
 
 export default MovieCard;
